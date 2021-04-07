9bb929e4856304a7fb61c3017f2ba6d7a7560855
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-04-07 17:04:41 +0200

    milestone 2 e 3 da correggere

--- js/main.js
+++ js/main.js
@@ -2,31 +2,26 @@ var app = new Vue ({
     el: "#app",
     data: {
         moviesTv: [],
-        find: ""
-    },
-    mounted() {
+        find: "",
         
     },
-
     methods: {
         srch() {
             axios
                 .get("https://api.themoviedb.org/3/search/movie?api_key=14c79cb15a17f3680279c4003fdc0fdc&query=" + this.find)
-                .then((result) => {                              
-                    this.moviesTv.forEach(element => {
-                        element.vote_average = Math.ceil(parseInt(element.vote_average) / 2);
-                    });
-                    this.moviesTv = this.moviesTv.concat(result.data.results); 
+                .then((result) => {       
+                    this.moviesTv = this.moviesTv.concat(result.data.results);  
                 });
                             
             axios
                 .get("https://api.themoviedb.org/3/search/tv?api_key=14c79cb15a17f3680279c4003fdc0fdc&query=" + this.find)
-                .then((result) => {                            
-                    this.moviesTv.forEach(element => {
-                        element.vote_average = Math.ceil(parseInt(element.vote_average) / 2);
-                    });
-                    this.moviesTv = this.moviesTv.concat(result.data.results); 
-                });   
+                .then((result) => {                                    
+                    this.moviesTv = this.moviesTv.concat(result.data.results);      
+                });
+            this.moviesTv.forEach(element => {
+                element.vote_average = Math.ceil(parseInt(element.vote_average) / 2);
+                element.poster_path = ("https://image.tmdb.org/t/p/w300" + element.poster_path);
+            });    
         }
     } 
 })

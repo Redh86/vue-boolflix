45bea7e9eb1703744815abdc831a0abcf9458862
Dario Tedeschi <dario.tedeschi86@hotmail.it>
2021-04-07 11:32:05 +0200

    milestone 2 in progress

--- js/main.js
+++ js/main.js
@@ -1,20 +1,32 @@
 var app = new Vue ({
     el: "#app",
     data: {
-        movies: [],
+        moviesTv: [],
         find: ""
+    },
+    mounted() {
+        
     },
 
     methods: {
         srch() {
-            const self = this
             axios
                 .get("https://api.themoviedb.org/3/search/movie?api_key=14c79cb15a17f3680279c4003fdc0fdc&query=" + this.find)
-                .then((result) => {
-                    console.log(result)
-                this.movies = result.data.results;
-                console.log(this.movies)
-            })   
+                .then((result) => {                              
+                    this.moviesTv.forEach(element => {
+                        element.vote_average = Math.ceil(parseInt(element.vote_average) / 2);
+                    });
+                    this.moviesTv = this.moviesTv.concat(result.data.results); 
+                });
+                            
+            axios
+                .get("https://api.themoviedb.org/3/search/tv?api_key=14c79cb15a17f3680279c4003fdc0fdc&query=" + this.find)
+                .then((result) => {                            
+                    this.moviesTv.forEach(element => {
+                        element.vote_average = Math.ceil(parseInt(element.vote_average) / 2);
+                    });
+                    this.moviesTv = this.moviesTv.concat(result.data.results); 
+                });   
         }
     } 
 })
